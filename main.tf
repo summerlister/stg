f0d474079e6887bc0742f00d2ae39635d0b15267
summerlister <summerlister@microsoft.com>
2022-10-03 11:20:05 -0400

    first commit

---
+++ main.tf
@@ -0,0 +1,55 @@
+#############################################################################
+# PROVIDERS
+#############################################################################
+
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "~>3.18"
+#     }
+#     # random = {
+#     #   source  = "hashicorp/random"
+#     #   version = "~>3.1"
+#     # }
+#   }
+# }
+
+
+# provider "azurerm" {
+#   features {}
+# }
+
+# provider "azurerm" {
+#   alias           = "HUB"
+#   subscription_id = var.HUB_SUBSCRIPTION
+#   tenant_id       = var.HUB_TENANTID
+#   client_id       = var.HUB_CLIENTID
+#   client_secret   = var.HUB_CLIENTSECRET
+#   features {}
+# }
+
+# provider "random" {
+# }
+
+
+#############################################################################
+# RESOURCES
+#############################################################################
+
+resource "azurerm_storage_account" "stg" {
+  name                = var.STG_NAME
+  resource_group_name = var.RESOURCE_GROUP
+
+  location                 = var.LOCATION
+  account_tier             = var.TIER
+  account_replication_type = var.REPLICATION
+
+  network_rules {
+    default_action             = var.NETWORK_ACTION
+    ip_rules                   = var.ALLOW_IP_LIST
+    virtual_network_subnet_ids = var.ALLOW_VNET_LIST
+  }
+
+  tags = var.TAGS
+}
---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "STG_NAME" {
+  description = ""
+}
+variable "RESOURCE_GROUP" {
+  description = ""
+}
+variable "LOCATION" {
+  description = ""
+}
+variable "TIER" {
+  description = ""
+}
+variable "REPLICATION" {
+  description = ""
+}
+variable "NETWORK_ACTION" {
+  default = "Deny"
+  description = ""
+}
+variable "ALLOW_IP_LIST" {
+  default = []
+  description = ""
+}
+variable "ALLOW_VNET_LIST" {
+  default = []
+  description = ""
+}
+variable "TAGS" {
+  description = ""
+}
